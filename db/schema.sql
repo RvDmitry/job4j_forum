be9de9973facbb138325919bdb7825a2f2871702
RvDmitry <rvdmitry@gmail.com>
2021-04-21 15:42:12 +0600

    1. Spring mock [#407130]

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ create table posts (
    id serial primary key,
    name varchar(2000),
    description text,
-   created timestamp not null
+   created timestamp not null default now()
 );
 
 CREATE TABLE authorities (
